b54b3e3c3e5043e092823f66c0ba403353ac4997
Dmitry-Ostashev <dmitry.ostashev@devexpress.com>
2022-06-23 18:28:33 +0400

    handle error

--- tests/index.js
+++ tests/index.js
@@ -6,5 +6,9 @@ const DEFAULT_HOSTNAME    = 'localhost';
 const concurrency = process.argv[3] && parseInt(process.argv[3]) || DEFAULT_CONCURRENCY;
 const hostname    = process.argv[2] || DEFAULT_HOSTNAME;
 
-runTests(`http://${hostname}:5002/`, concurrency, true);
+runTests(`http://${hostname}:5002/`, concurrency, true)
+    .catch(err => {
+        console.log(err);
+        process.exit(1);
+    });
 //Why puppeteer-cluster instead of puppeteer-loadtest? Supports the latest puppeteer version, zero vulnerabilities
