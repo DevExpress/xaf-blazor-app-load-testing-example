87dcd07627db4d0085de76b52664a811934f3bff
Dmitry-Ostashev <dmitry.ostashev@devexpress.com>
2022-06-23 20:18:40 +0400

    try with testcafe

---
+++ tests/list-view-testcafe-test.js
@@ -0,0 +1,17 @@
+import { Selector } from 'testcafe';
+
+fixture`List View`
+    .page`localhost:5000`;
+
+for (let i = 0; i < 30; i ++) {
+    test(`List View Pages Navigation - ${i}`, async t => {
+        await t.expect(Selector('.dxbs-grid .card').exists).ok({ timeout: 30000 });
+
+        for (let j = 0; j < 19; j++) {
+            await t
+                .click('[data-args="PBN"] div svg')
+                .expect(Selector('.dxbs-grid .page-link input').value).eql(`${j + 2}`)
+                .wait(500);
+        }
+    });
+}
