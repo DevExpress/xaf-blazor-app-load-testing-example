f1eafb8beb2316256970986255b6d44e34c1a0b4
Dmitry-Ostashev <dmitry.ostashev@devexpress.com>
2022-07-08 10:19:52 +0400

    rework tests

--- tests/detail-view-test.js
+++ tests/detail-view-test.js
@@ -2,9 +2,10 @@ async function detailViewTest(page) {
     for (let i = 0; i < 20; i++) {
         await page.waitForSelector('.dxbs-grid .card');
         await page.waitForXPath(`//td[contains(., 'Employee ${i}#')]`);
-        await page.waitForTimeout(1000);
 
         const row = await page.$x(`//td[contains(., 'Employee ${i}#')]`);
+
+        await page.waitForTimeout(500);
 
         await row[0].click();
 
